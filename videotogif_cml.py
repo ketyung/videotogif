adfd146fff620a8b2e85a6ebb566ffd0ecb619df
ketyung <67858418+ketyung@users.noreply.github.com>
2020-12-17 20:36:34 +0800

    Add files via upload

---
+++ videotogif_cml.py
@@ -0,0 +1,43 @@
+from moviepy.editor import VideoFileClip
+import sys 
+
+if len(sys.argv) < 2 :
+	print("Usage : python videotogif_cml.py [video_path] [fps] [time_start] [time_end] [resize_to] [ouput_path]")
+	sys.exit()
+else:
+	print("Start converting...")
+	videoclip = VideoFileClip( sys.argv[1])
+
+	fps = 10
+
+	if len(sys.argv) > 2:
+		fps = int(sys.argv[2])
+
+	if len(sys.argv) > 4:
+		time_start = float(sys.argv[3])
+		time_end = float(sys.argv[4])
+		videoclip = videoclip.subclip(time_start,time_end)
+		
+	elif len(sys.argv) > 3:
+		time_start = float(sys.argv[3])
+		videoclip = videoclip.subclip(time_start)
+		
+		
+	if len(sys.argv) > 5:
+		videoclip = videoclip.resize(float(sys.argv[5]))
+		
+		
+		
+		
+	output_path = "output.gif"
+
+	if len(sys.argv) > 6:
+		output_path = sys.argv[6]
+
+	videoclip.write_gif(output_path, fps=fps)
+	
+	
+
+	
+	
+
